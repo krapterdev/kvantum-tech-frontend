1e5f93d8e0ad99a247faa8f9851dd602db5b5a3b
Krapterdev <krapter.dev@gmail.com>
2026-08-18 00:34:04 +0530

    fix: remove invalid next/dynamic ssr false from admin routes to fix App Router rendering

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import dynamic from 'next/dynamic';
-
-const AdminClient = dynamic(() => import('./AdminClient'), { ssr: false });
+import AdminClient from './AdminClient';
 
 export default function AdminPage() {
   return <AdminClient />;
